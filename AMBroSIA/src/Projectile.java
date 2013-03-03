884dc261a1159491894fc9a8c7fb4f909cabfdd8
Ryuke <ac.91@live.ca>
2013-03-03 14:13:51 -0500

    Added methods

--- AMBroSIA/src/Projectile.java
+++ AMBroSIA/src/Projectile.java
@@ -7,8 +7,22 @@
  *
  * @author Anthony
  */
+
+import javax.swing.ImageIcon;
+
 public class Projectile extends MapObject {
-    public Projectile(int velocity, int heading, int[] coordinates, ImageIcon img, GameState gs, int ttl){     
-        
+    private int velocity; // to be fixed
+    private int ttl;
+    
+    public Projectile(int heading, int[] coordinates, ImageIcon img, int velocity, GameState gs, int ttl){     
+        super(heading,coordinates,img);
+        this.ttl = ttl;      
+    }
+    
+    public int getTTL(){
+        return this.heading;
+    }
+    public void setTTL(int ttl){
+        this.ttl = ttl;
     }
 }
