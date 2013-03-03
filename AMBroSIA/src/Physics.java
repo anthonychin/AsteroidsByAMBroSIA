fbd33897de6bfb701bb308c47e993a6c498b8bd8
Michael Smith <michael.smith6@mail.mcgill.ca>
2013-03-03 11:35:28 -0500

    Filling out method headers.

--- AMBroSIA/src/Physics.java
+++ AMBroSIA/src/Physics.java
@@ -8,5 +8,11 @@
  * @author Nikolaos
  */
 public class Physics {
+    public void update(MapObject mo);
     
+    public void update(Ship s);
+    
+    public List<Trajectory> getTrajectory(AlienShip as);
+    
+    public void checkCollision(MapObject PlayerShip, List <MapObject> AlienShip, List<MapObject> Asteroids, List<MapObject> Projectiles, List<MapObject> BonusDrops);
 }
