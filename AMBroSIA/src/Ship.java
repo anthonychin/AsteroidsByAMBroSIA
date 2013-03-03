b0032777093d8e39e664665765fac5dbb633351e
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2013-03-03 11:51:14 -0500

    Added method descriptions to Ship.java

--- AMBroSIA/src/Ship.java
+++ AMBroSIA/src/Ship.java
@@ -8,5 +8,34 @@
  * @author Nikolaos
  */
 public class Ship {
+    private int lives;
+    private int fireRate;
+    Ship(int fireRate, int lives)
+    {
+    }
     
+    public int getFireRate()
+    {
+        return this.fireRate;
+    }
+    
+    public void setFireRate(int fireRate)
+    {
+        
+    }
+    
+    public int getLives()
+    {
+        return this.lives;
+    }
+    
+    public void setLives(int lives)
+    {
+        
+    }
+    
+    public void shoot()
+    {
+        
+    }
 }
