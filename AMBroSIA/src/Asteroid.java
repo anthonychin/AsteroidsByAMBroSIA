b045003d62f620009ed254d7920f1c8c91bbf3d8
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2013-03-03 15:02:39 -0500

    Added destroy()

--- AMBroSIA/src/Asteroid.java
+++ AMBroSIA/src/Asteroid.java
@@ -5,6 +5,10 @@
 import java.awt.Polygon;
 
 public class Asteroid extends MapObject{
+    final public static int LARGE_ASTEROID_SIZE = 3;
+    final public static int MEDIUM_ASTEROID_SIZE = 2;
+    final public static int SMALL_ASTEROID_SIZE = 1;
+    
     private int size;
     
     Asteroid(int velocity, int heading, int[] coordinates, Polygon shape, GameState gameState, int size)
@@ -16,5 +20,23 @@ public class Asteroid extends MapObject{
     public int getSize()
     {
         return this.size;
+    }
+    
+    public void destroy()
+    {
+        getGameState().removeAsteroid(this);
+        if(size == 3)
+        {
+            getGameState().addAsteroid(new Asteroid(this.getVelocity(), this.getHeading(), this.getCoord, this.getShape(), this.getGameState, 2));
+            
+        }
+        else if(size == 2)
+        {
+            
+        }
+        else
+        {
+            
+        }
     }
 }
