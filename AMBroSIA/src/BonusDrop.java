d07a08517e6be261b8457bd38882d2ae682843c7
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2013-03-03 14:39:55 -0500

    Added methods to BonusDrop according to SAD 1.0

--- AMBroSIA/src/BonusDrop.java
+++ AMBroSIA/src/BonusDrop.java
@@ -7,6 +7,26 @@
  *
  * @author Nikolaos
  */
+import java.awt.Polygon;
+
 public class BonusDrop extends MapObject {
+    private int type;
+    private int ttl;
     
+    BonusDrop(int[] coordinates, Polygon shape, GameState gameState, int type, int ttl)
+    {
+        super(0, 0, coordinates, shape, gameState);
+        this.type = type;
+        this.ttl = ttl;
+    }
+    
+    public int getType()
+    {
+        return this.type;
+    }
+    
+    public int getTTL()
+    {
+        return this.ttl;
+    }
 }
