35d83a1f090a2a35235d3ef355d5764de3cd3581
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2013-03-03 19:56:49 -0500

    Changed velocity from int to int[]

--- AMBroSIA/src/AlienShip.java
+++ AMBroSIA/src/AlienShip.java
@@ -12,7 +12,7 @@ import java.awt.Polygon;
 
 public class AlienShip extends Ship{
     
-    public AlienShip(int velocity, int heading, int[] coordinates, GameState gameState, int fireRate, int lives){
+    public AlienShip(int[] velocity, int heading, int[] coordinates, GameState gameState, int fireRate, int lives){
         super(velocity, heading, coordinates, gameState, fireRate, lives);
     }
     
