6f96446ae378fab5164b49c92f6fcae429c5c128
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2013-03-04 10:36:06 -0500

    Fixed acceleration parameters for constructor Added owner property and its setting logic in constructor

--- AMBroSIA/src/Projectile.java
+++ AMBroSIA/src/Projectile.java
@@ -6,14 +6,22 @@
 public class Projectile extends MapObject {
     public final static int[] PROJECTILE_VELOCITY = {10, 10};
     public final static int TIME_TO_LIVE = 5; //5 seconds
+    public final static int PLAYER_OWNER = 1;
+    public final static int ALIEN_OWNER = 2;
     
     private int velocity; // to be fixed
     private int ttl;
+    private int owner;
     
     
-    public Projectile(int[] velocity, int heading, int[] coordinates, GameState gameState){     
-        super(velocity, heading, coordinates, gameState);
-        this.ttl = TIME_TO_LIVE;      
+    public Projectile(Ship ship, int[] velocity, int heading, int[] coordinates, GameState gameState){     
+        super(velocity, heading, coordinates, 0, gameState);
+        this.ttl = TIME_TO_LIVE;
+        
+        if(ship instanceof PlayerShip)
+            this.owner = PLAYER_OWNER;
+        else
+            this.owner = ALIEN_OWNER;
     }
     
     public int getTTL(){
