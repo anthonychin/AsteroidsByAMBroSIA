44ecd880dfa85bcf79475838ff4bad272c60fba9
meongseo <meong.seo@mail.mcgill.ca>
2013-03-06 01:45:53 -0500

    Sound.java tested

--- AMBroSIA/src/Sound.java
+++ AMBroSIA/src/Sound.java
@@ -1,15 +1,14 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  *
- * @author Myong
+ * @author Meong Hee
  */
 public class Sound {
     
@@ -17,49 +16,29 @@ public class Sound {
     private AudioInputStream sound;
     private Clip clip;
     
-//    private AudioClip background;
 
-    public Sound(String inputFile) {
+    //  The string inputFile should contain the Path of the sound file.
+    public Sound(String inputFile) throws UnsupportedAudioFileException, 
+            IOException, LineUnavailableException {
         file = new File(inputFile);
-        try {
-            sound = AudioSystem.getAudioInputStream(file);
-        } catch (UnsupportedAudioFileException ex) {
-            Logger.getLogger(Sound.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(Sound.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        
+        sound = AudioSystem.getAudioInputStream(file);
+        clip = AudioSystem.getClip();
+        clip.open(sound);
     }
-//    //  This method will play the background music.
-//    public void playBackgroundSound() throws Exception {
-//        backgroundFile = new File("sound/background.wav");
-//        background = AudioSystem.getAudioInputStream(backgroundFile);
-////        background = getAudioClip(getClass().getResources(), "sounds/background.wav");
-//    }
-//    
-//    //  This method will play the sound of the gun shooting.
-//    public void playGunSound();
-//    
-//    //  This method will play the sound of the bomb exploding.
-//    public void playBombSound();
-//    
-//    //  This method will play the main theme.
-//    public void playTheme();
-//    
-//    //  This method will play the shield sound.
-//    public void playShieldSound();
-//    
-//    //  This method will play the sound of the thruster firing.
-//    public void playThruster();
-//    
-//    //  This method will play the sound of an explosion.
-//    public void playExplosion();
-//    
-//    //  This method will play the victory sound.
-//    public void playVictory();
-//    
-//    //  This will play the Game Over sound.
-//    public void playDefeated();
-//
+    
+    //  Plays the clip only once.
+    public void play() {
+        clip.setFramePosition(0);
+        clip.start();
+    }
+    
+    //  Stops the currently playing clip.
+    public void stop() {
+        clip.stop();
+    }
+    
+    //  Plays the clip in continuously. e.g. BGM
+    public void playLoop() {
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
 }
