19e4db98e4fe73d350b1f66dbf3dd8b58cde5b08
Michael Smith <michael.smith6@mail.mcgill.ca>
2013-03-14 16:59:01 -0400

    Improved graphics quality with antialiasing and other settings.

--- AMBroSIA/src/gui/gameDraw.java
+++ AMBroSIA/src/gui/gameDraw.java
@@ -8,7 +8,9 @@ import game.MapObject;
 import game.Projectile;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  *
@@ -18,9 +20,19 @@ public class gameDraw {
     
     public static void drawObjects(Graphics2D g2d, GameState memory)
     {
+        //some nice settings that improve visual quality.  Some do not appear to have an effect however...
+        g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_LCD_CONTRAST, 100);
+        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+
         //draw asteroids
-        if (!memory.getAsteroids().isEmpty())
-        {
+        if (!memory.getAsteroids().isEmpty()) {
             ArrayList<Asteroid> asteroidList = memory.getAsteroids();
             for (Asteroid asteroid : asteroidList)
             {
