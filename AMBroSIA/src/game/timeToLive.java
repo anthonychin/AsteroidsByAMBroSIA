2d68e45bb69a10ca7007d3abdbc2282208bff63e
Michael Smith <michael.smith6@mail.mcgill.ca>
2013-03-19 20:46:18 -0400

    Finally fixed the time to live for loop getting stuck issue.  Had to use an iterator.

--- AMBroSIA/src/game/timeToLive.java
+++ AMBroSIA/src/game/timeToLive.java
@@ -1,17 +1,21 @@
 package game;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import javax.swing.SwingUtilities;
 
 /**
- * The Time to live class contains code that checks the time to live of certain objects (such as bonus drops, projectiles) and removes them from the game once it is time for them to be removed.
+ * The Time to live class contains code that checks the time to live of certain
+ * objects (such as bonus drops, projectiles) and removes them from the game
+ * once it is time for them to be removed.
+ *
  * @author Michael
  */
-public class timeToLive implements Runnable{
-    
+public class timeToLive implements Runnable {
+
     private GameState gameState;
-    
-    public timeToLive(GameState gs)
-    {
+
+    public timeToLive(GameState gs) {
         gameState = gs;
     }
 
@@ -19,33 +23,30 @@ public class timeToLive implements Runnable{
     public void run() {
         checkTTL();
     }
-    
-    private void checkTTL()
-    {
+
+    private void checkTTL() {
         //work on all bonus drops, explosions, projectiles
         TTLLogic(gameState.getBonusDrops());
         TTLLogic(gameState.getExplosions());
         TTLLogic(gameState.getProjectiles());
     }
-    
-    private void TTLLogic(ArrayList<? extends MapObjectTTL> list)
-    {
-        if (!list.isEmpty())
-        {
-            for (MapObjectTTL object : list)
-            {
-                //if expired, delete, else, decrement counter
-                if (object.getTTL() > 0)
-                {
-                    object.setTTL(object.getTTL()-1);
-                }
-                else
-                {
-                    object.destroy();
+
+    private void TTLLogic(ArrayList<? extends MapObjectTTL> list) {
+        if (!list.isEmpty()) {
+            //iterator needed because only the iterator.remove() can remove an object while in the loop.  Anything else will run into issues.
+            Iterator<? extends MapObjectTTL> iter = list.iterator();
+            while (iter.hasNext()) {
+                MapObjectTTL object = iter.next();
+                
+                //TTL is in seconds : if it still has life left we simply set it for the next one, else, it's gone
+                if (object.getTTL() > 0) {
+                    object.setTTL(object.getTTL() - 1);
+                } 
+                
+                else {
+                    iter.remove();
                 }
             }
         }
     }
-    
-    
 }
