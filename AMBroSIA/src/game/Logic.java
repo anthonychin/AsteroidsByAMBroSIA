6a35c28f855c8a6e34d3b3f63b6cb15c4a398457
Michael Smith <michael.smith6@mail.mcgill.ca>
2013-04-04 08:34:19 -0400

    Added escape key to game to return to main menu.

--- AMBroSIA/src/game/Logic.java
+++ AMBroSIA/src/game/Logic.java
@@ -242,6 +242,7 @@ public class Logic extends KeyAdapter implements ActionListener {
             //gameState.addAsteroid(new Asteroid(new float[]{Difficulty.randomAsteroidVelocity(10), Difficulty.randomHeading()}, randu.nextInt(360), new int[]{randu.nextInt(700), randu.nextInt(500)}, gameState, Asteroid.LARGE_ASTEROID_SIZE));
             //gameState.addProjectile(new Projectile(gameState.getAlienShip(), randu.nextInt(360), new int[] {gameState.getAlienShip().getX(), gameState.getAlienShip().getY()}, gameState));
         } else if (keyCode == KeyEvent.VK_ESCAPE) {
+            //may have to add if statement for two player here
             displayGameOver();
         }
     }
