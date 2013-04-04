3d2fa8f77f7a510a05d6e7df8f96dbd7e0687ec4
Michael Smith <michael.smith6@mail.mcgill.ca>
2013-04-04 08:33:28 -0400

    Added escape key to game to return to main menu.

--- AMBroSIA/src/game/Logic.java
+++ AMBroSIA/src/game/Logic.java
@@ -236,12 +236,13 @@ public class Logic extends KeyAdapter implements ActionListener {
             if (!paused && player != null) {
                 player.useBomb();
             }
-        }
-        if (keyCode == KeyEvent.VK_Z) {
+        } else if (keyCode == KeyEvent.VK_Z) {
             Random randu = new Random();
-            gameState.addAsteroid(new Asteroid(new float[]{1.5f, 1.5f}, randu.nextInt(360), new int[]{randu.nextInt(700), randu.nextInt(500)}, gameState, Asteroid.LARGE_ASTEROID_SIZE));            
+            gameState.addAsteroid(new Asteroid(new float[]{1.5f, 1.5f}, randu.nextInt(360), new int[]{randu.nextInt(700), randu.nextInt(500)}, gameState, Asteroid.LARGE_ASTEROID_SIZE));
             //gameState.addAsteroid(new Asteroid(new float[]{Difficulty.randomAsteroidVelocity(10), Difficulty.randomHeading()}, randu.nextInt(360), new int[]{randu.nextInt(700), randu.nextInt(500)}, gameState, Asteroid.LARGE_ASTEROID_SIZE));
             //gameState.addProjectile(new Projectile(gameState.getAlienShip(), randu.nextInt(360), new int[] {gameState.getAlienShip().getX(), gameState.getAlienShip().getY()}, gameState));
+        } else if (keyCode == KeyEvent.VK_ESCAPE) {
+            displayGameOver();
         }
     }
 
