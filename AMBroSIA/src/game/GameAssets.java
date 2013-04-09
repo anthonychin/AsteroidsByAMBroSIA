c262b6c244c0ece9238698580f9b62166e167707
Meong Hee <Meong Hee@142.157.150.13>
2013-04-09 10:13:39 -0400

    Javadoc

--- AMBroSIA/src/game/GameAssets.java
+++ AMBroSIA/src/game/GameAssets.java
@@ -16,30 +16,99 @@ import javax.swing.ImageIcon;
 public class GameAssets {
 
     //all sounds and images are accessible
+    /**
+     * Sound for victory.
+     */
     public static Sound victory;
+    /**
+     * Sound for game over.
+     */
     public static Sound gameOver;
+    /**
+     * Sound for no bomb.
+     */
     public static Sound noBombs;
+    /**
+     * Sound for bomb used.
+     */
     public static Sound bombUsed;
+    /**
+     * Sound for 3 shield points.
+     */
     public static Sound shields3;
+    /**
+     * Sound for 2 shield points.
+     */
     public static Sound shields2;
+    /**
+     * Sound for 1 shield point.
+     */
     public static Sound shields1;
+    /**
+     * Sound for no shield points left.
+     */
     public static Sound shields0;
+    /**
+     * Sound for crashing.
+     */
     public static Sound crash;
+    /**
+     * Sound for explosion.
+     */
     public static Sound explosion;
+    /**
+     * Sound for player firing.
+     */
     public static Sound playerFire;
+    /**
+     * Sound for alien firing.
+     */
     public static Sound alienFire;
+    /**
+     * Sound for theme.
+     */
     public static Sound theme;
+    /**
+     * Sound for missile.
+     */
     public static Sound missile;
+    /**
+     * Sound for alien saucer.
+     */
     public static Sound alienSaucer;
+    /**
+     * Sound for alien detection.
+     */
     public static Sound alienDetected;
+    /**
+     * Sound for thrusters.
+     */
     public static Sound thrusters;
+    /**
+     * Sound for warp.
+     */
     public static Sound warp;
     
+    /**
+     * Image for game background.
+     */
     public static Image spaceBackground;
+    /**
+     * Image for main menu.
+     */
     public static Image menuImage;
+    /**
+     * Image for tutorial.
+     */
     public static Image tutorialImage;
+    /**
+     * Image for game over.
+     */
     public static Image gameOverImage;
 
+    /**
+     * Load all the sounds.
+     */
     public static void loadSounds() {
         try {
             victory = new Sound("GoalsComplete.wav");
@@ -70,6 +139,9 @@ public class GameAssets {
         }
     }
     
+    /**
+     * Loads all images.
+     */
     public static void loadImages() {
         spaceBackground = new ImageIcon(GameAssets.class.getResource("images/spaceBackground.jpg")).getImage();
         tutorialImage = new ImageIcon(GameAssets.class.getResource("images/keyboard.jpg")).getImage();
