101583373486cd46d605e4f5b2d96421057735c1
Meong Hee <Meong Hee@142.157.150.13>
2013-04-09 13:53:33 -0400

    Javadoc

--- AMBroSIA/src/game/BonusDrop.java
+++ AMBroSIA/src/game/BonusDrop.java
@@ -2,20 +2,41 @@ package game;
 
 /**
  * The bonus drop object.
- * @author Nikolaos
+ * @author Nikolaos, Meong Hye Seo
  */
 public class BonusDrop extends MapObjectTTL {
 
+    /**
+     * An int value that declares the type of bonus drop to bomb.
+     */
     final public static int BOMB_BONUS_DROP = 0;
+    /**
+     * An int value that declares the type of bonus drop to life.
+     */
     final public static int LIFE_BONUS_DROP = 1;
+    /**
+     * An int value that declares the type of bonus drop to 3 shield points.
+     */
     final public static int SHIELD_THREE_POINTS_DROP = 2;
+    /**
+     * An int value that declares the type of bonus drop to 2 shield points.
+     */
     final public static int SHIELD_TWO_POINTS_DROP = 3;
+    /**
+     * An int value that declares the type of bonus drop to 1 shield point.
+     */
     final public static int SHIELD_ONE_POINT_DROP = 4;
     private int type;
     //10 sec.
     final private int TIME_TO_LIVE = 25;
 
     //create bonus drop
+    /**
+     * Creates bonus drop with given parameters.
+     * @param coordinates initial X, Y position of the bonus drop
+     * @param gameState current game state
+     * @param type type of bonus drop
+     */
     public BonusDrop(int[] coordinates, GameState gameState, int type) {
         super(new float[]{0, 0}, 0, coordinates, 0, gameState);
         this.type = type;
@@ -23,6 +44,10 @@ public class BonusDrop extends MapObjectTTL {
     }
 
     //the type of bonus drop
+    /**
+     * Returns the type of bonus drop.
+     * @return int value representing type of bonus drop
+     */
     public int getType() {
         return this.type;
     }
