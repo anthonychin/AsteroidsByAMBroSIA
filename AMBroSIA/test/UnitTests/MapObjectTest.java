0853882d057b0aa459769de5653a67a5a13315ed
Ryuke <ac.91@live.ca>
2013-04-10 01:08:56 -0400

    unit tests

---
+++ AMBroSIA/test/UnitTests/MapObjectTest.java
@@ -0,0 +1,75 @@
+
+package UnitTests;
+
+import game.MapObject;
+import game.GameAssets;
+import game.GameState;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ * Test the very basic map objects
+ * @author Anthony Chin
+ */
+public class MapObjectTest {
+    private GameState gameState;
+    private MapObject obj;
+    
+    @Before
+    public void setUp() {
+        gameState = new GameState(); 
+        GameAssets.loadSounds();
+        obj = new MapObject(new float[]{1.5f, 1.5f}, 350f, new int[]{650, 450},4, gameState);
+    }
+    
+    @After
+    public void tearDown(){
+        gameState = null;
+        obj = null;
+    }
+    
+    @Test
+    public void getVelocity(){
+        assertEquals("Test velocity as a floating point value", obj.getVelocity()[0], 1.5f, 0.000000005);
+        assertEquals("Test velocity as a floating point value", obj.getVelocity()[1], 1.5f, 0.000000005);        
+    }
+    
+    @Test
+    public void setVelocity(){
+        obj.setVelocity(new float[]{1.6f,1.6f});
+        assertEquals("Test velocity as a floating point value", obj.getVelocity()[0], 1.6f, 0.000000005);
+        assertEquals("Test velocity as a floating point value", obj.getVelocity()[1], 1.6f, 0.000000005);        
+    }    
+    
+    @Test
+    public void getX(){
+        assertEquals("x position", obj.getX(), 650);
+    }
+    
+    @Test
+    public void getY(){
+        assertEquals("y position", obj.getY(), 450);
+    }
+    
+    @Test
+    public void setX(){
+        obj.setX(20);
+        assertEquals("x position", obj.getX(), 20);
+    }
+    
+    @Test
+    public void setY(){
+        obj.setY(20);
+        assertEquals("y position", obj.getY(), 20);
+    }
+    
+    @Test
+    public void getGameState(){
+        obj.getGameState();
+        assertNotNull("map object should be in gameState", obj.getGameState());
+    }
+    
+}
---
+++ AMBroSIA/test/UnitTests/ProjectileTest.java
@@ -0,0 +1,59 @@
+package UnitTests;
+
+import game.AlienShip;
+import game.Projectile;
+import game.GameAssets;
+import game.GameState;
+import game.PlayerShip;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ * Projectile Test Cases
+ * @author Anthony Chin
+ */
+public class ProjectileTest {
+    private GameState gameState;
+    private Projectile p;
+    private Projectile p2;
+    private PlayerShip ps;
+    private AlienShip as;
+    
+    @Before
+    public void setUp(){
+        gameState = new GameState();
+        GameAssets.loadSounds();        
+        ps = new PlayerShip(new float[]{0, 0}, 0, new int[]{800, 600}, gameState, 3, 1, 3);
+        as = new AlienShip(new float[]{1.5f,1.5f}, 20, new int[]{800,600}, gameState);
+        p = new Projectile(ps, 350f, new int[]{600, 500},gameState);
+        p2 = new Projectile(as, 350f, new int[]{610, 510},gameState);
+        
+    }
+    
+    @After
+    public void tearDown(){
+        gameState = null;
+        p = null;
+        p2 = null;
+    }
+    
+    @Test
+    public void getOwner(){
+        gameState.addProjectile(p);
+        gameState.addProjectile(p2);
+
+        assertEquals("Player Projectile", p.getOwner(), 1);
+        assertEquals("Alien Projectile", p2.getOwner(), 2);
+
+    }
+    
+    @Test
+    public void destroyProjectile(){
+        gameState.addProjectile(p);
+        p.destroy();
+        assertEquals("projectile is gone", gameState.getProjectiles().size(), 0);
+    }
+}
