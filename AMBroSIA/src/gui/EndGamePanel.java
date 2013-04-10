6deba5a5d958f72f3ba9f52353e1d60722756163
Meong Hee <Meong Hee@142.157.150.13>
2013-04-10 11:16:35 -0400

    Javadoc

--- AMBroSIA/src/gui/EndGamePanel.java
+++ AMBroSIA/src/gui/EndGamePanel.java
@@ -2,21 +2,17 @@ package gui;
 
 import game.GameState;
 import highscoreData.highScoreWriter;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 
 /**
- * game over screen
+ * The
+ * <code>EndGamePanel</code> class displays game over screen.
  *
  * @author Haisin Yip
  *
@@ -30,6 +26,15 @@ public class EndGamePanel extends JPanel {
     private Image img;
 
     // initialize size, layout and informative display
+    /**
+     * Creates EndGamePanel using given parameters. It initializes size, layout
+     * and informative display.
+     *
+     * @param img image for EndGamePanel
+     * @param gs current game state
+     * @param singleP boolean value representing single player mode or 2 player
+     * mode.
+     */
     public EndGamePanel(Image img, GameState gs, boolean singleP) {
         this.gamestate = gs;
 
@@ -40,7 +45,7 @@ public class EndGamePanel extends JPanel {
         setMaximumSize(size);
         setSize(size);
         setLayout(null);
-        
+
         makeComponents(getWidth(), getHeight(), singleP);
         makeLayout();
     }
@@ -65,7 +70,7 @@ public class EndGamePanel extends JPanel {
             //player 1, player 2 scores
             int highscoreP1 = gamestate.getPlayer1Score();
             String levelP1 = String.valueOf(gamestate.getPlayer1Level());
-        
+
             int highscoreP2 = gamestate.getPlayer2Score();
             String levelP2 = String.valueOf(gamestate.getPlayer2Level());
 
@@ -73,12 +78,12 @@ public class EndGamePanel extends JPanel {
             if (highscoreP1 >= highscoreP2) {
                 player = "p1";
                 highscore = String.valueOf(highscoreP1);
-                
+
                 level = levelP1;
             } else {
                 player = "p2";
                 highscore = String.valueOf(highscoreP2);
-                
+
                 level = levelP2;
             }
         }
@@ -87,9 +92,9 @@ public class EndGamePanel extends JPanel {
         String[] columnData = {"", ""};
         String[][] rowData = {{"Player name", player}, {"Highscore", highscore}, {"Asteroids Destroyed", "asteroidsDestroyed"}, {"Aliens Destroyed", "aliensDestroyed"}, {"Kill-Death ratio", "killDeathRatio"}, {"Last level", level}, {"Bombs used", "bombs"}, {"Shooting Accuracy", "shootingAccuracy"}};
         StatisticsTable = new JTable(rowData, columnData);
-        StatisticsTable.setPreferredScrollableViewportSize(new Dimension(w/2, h/6));
-        StatisticsTable.setFillsViewportHeight(true);  
-       
+        StatisticsTable.setPreferredScrollableViewportSize(new Dimension(w / 2, h / 6));
+        StatisticsTable.setFillsViewportHeight(true);
+
         String[] scoreData = {player + " ", highscore + " ", "asteroidsdestroyed ", "aliensdestroyed ", "Kill-Deathratio ", level + " ", "bombs ", "shootingaccuracy"};
         highScoreWriter writer = new highScoreWriter(scoreData, "./src/highscoreData/scoreInfo.txt");
         writer.writeToFile();
@@ -101,8 +106,13 @@ public class EndGamePanel extends JPanel {
         add(StatisticsTable);
         add(new JScrollPane(StatisticsTable));
     }
-    
+
     // set endgame background image
+    /**
+     * Sets endgame background image.
+     *
+     * @param g image for background
+     */
     @Override
     public void paintComponent(Graphics g) {
         g.drawImage(img, 0, 0, getWidth(), getHeight(), null);
