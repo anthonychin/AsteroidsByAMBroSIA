661ef13869cbb861086f1caea4608e8deae72b8d
Meong Hee <Meong Hee@142.157.150.13>
2013-04-10 12:39:59 -0400

    Javadoc

--- AMBroSIA/src/gui/MenuPanel.java
+++ AMBroSIA/src/gui/MenuPanel.java
@@ -7,7 +7,9 @@ import java.awt.Image;
 import javax.swing.JPanel;
 
 /**
- * Draws the image for the menu
+ * The
+ * <code>MenuPanel</code> class draws the image for the menu.
+ *
  * @author Haisin Yip
  * @author Anthony Chin
  */
@@ -16,6 +18,9 @@ public class MenuPanel extends JPanel {
     private Image img = GameAssets.menuImage;
 
     //set up image location, size, etc.
+    /**
+     * Constructor which sets up image location, size, etc.
+     */
     public MenuPanel() {
         Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
         setPreferredSize(size);
@@ -25,6 +30,11 @@ public class MenuPanel extends JPanel {
     }
 
     //draw image
+    /**
+     * Draws image.
+     *
+     * @param g graphics
+     */
     @Override
     public void paintComponent(Graphics g) {
         g.drawImage(img, 0, 0, MenuGUI.WIDTH, MenuGUI.HEIGHT, this);
