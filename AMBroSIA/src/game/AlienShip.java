4275b7d746acbbe0b74d4018c7c42bdf82001248
Meong Hee <Meong Hee@142.157.150.13>
2013-04-10 09:48:54 -0400

    Javadoc

--- AMBroSIA/src/game/AlienShip.java
+++ AMBroSIA/src/game/AlienShip.java
@@ -12,6 +12,13 @@ public class AlienShip extends Ship {
     final private static int NUM_DEBRIS = 20;
 
     //create alien
+    /**
+     * Create AlienShip using given parameters.
+     * @param velocity magnitude and direction of the alien ship
+     * @param heading heading of the alien ship
+     * @param coordinates initial X, Y position of the alien ship
+     * @param gameState current game state
+     */
     public AlienShip(float[] velocity, float heading, int[] coordinates, GameState gameState) {
         super(velocity, heading, coordinates, 0, gameState, 1);
         //play alien entering system sound
@@ -19,6 +26,10 @@ public class AlienShip extends Ship {
     }
 
     //remove alien from game: add bonus drop where it was destroyed, play destruction sound, add sto core
+    /**
+     * Removes alien from game. Adds bonus drop where it was destroyed, plays destruction sound, and adds score.
+     * @param bombUsed true if bomb was used false otherwise
+     */
     public void destroy(boolean bombUsed) {
         int[] lastCoord = getCoord();
         getGameState().removeAlienShip();
