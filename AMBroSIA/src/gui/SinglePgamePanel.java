8f614197d88fe63733132c56165c21666403fee0
Meong Hee <Meong Hee@142.157.150.13>
2013-04-11 11:09:29 -0400

    Javadoc and variable names changed

--- AMBroSIA/src/gui/SinglePgamePanel.java
+++ AMBroSIA/src/gui/SinglePgamePanel.java
@@ -22,12 +22,12 @@ public class SinglePgamePanel extends DescriptionPanel {
     /**
      * Creates SinglePgamePanel with given parameters.
      *
-     * @param gs current game state
+     * @param gameState current game state
      * @param img image for single player game panel
      */
-    public SinglePgamePanel(GameState gs, Image img) {
-        super(gs);
-        this.gameState = gs;
+    public SinglePgamePanel(GameState gameState, Image img) {
+        super(gameState);
+        this.gameState = gameState;
 
         this.img = img;
         setLayout(null);
@@ -36,24 +36,24 @@ public class SinglePgamePanel extends DescriptionPanel {
     /**
      * Creates SinglePgamePanel with GameState.
      *
-     * @param gs current game state
+     * @param gameState current game state
      */
-    public SinglePgamePanel(GameState gs) {
-        super(gs);
-        this.gameState = gs;
+    public SinglePgamePanel(GameState gameState) {
+        super(gameState);
+        this.gameState = gameState;
     }
 
     // paints content onto the Single-Player mode panel
     @Override
-    public void paint(Graphics g) {
+    public void paint(Graphics graphic) {
         //don't draw outside screen
         super.setBounds(0, 0, MenuGUI.WIDTH, MenuGUI.HEIGHT);
 
         //perform all required swing painting functions
-        super.paint(g);
+        super.paint(graphic);
 
         //draw all on screen objects
-        Graphics2D g2 = (Graphics2D) g;
+        Graphics2D g2 = (Graphics2D) graphic;
         gameDraw.drawObjects(g2, gameState);
 
         //seems to reduce stuttering a bit. See http://docs.oracle.com/javase/7/docs/api/java/awt/Toolkit.html#sync%28%29
@@ -72,10 +72,10 @@ public class SinglePgamePanel extends DescriptionPanel {
     /**
      * Draws a background (only at the beginning).
      *
-     * @param g graphics
+     * @param graphic graphics
      */
     @Override
-    public void paintComponent(Graphics g) {
-        g.drawImage(img, 0, 0, getWidth(), getHeight(), null);
+    public void paintComponent(Graphics graphic) {
+        graphic.drawImage(img, 0, 0, getWidth(), getHeight(), null);
     }
 }
