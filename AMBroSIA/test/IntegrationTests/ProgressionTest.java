9b999c89e1294e32d59ba21f5f18bc2ae7aa3f4a
Ryuke <ac.91@live.ca>
2013-04-13 22:57:31 -0400

    testcases

---
+++ AMBroSIA/test/IntegrationTests/ProgressionTest.java
@@ -0,0 +1,17 @@
+package IntegrationTests;
+
+import game.*;
+import mapObjects.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Progression Test Cases
+ * @author Anthony Chin
+ */
+public class ProgressionTest {
+    private GameState gameState;
+    
+}
