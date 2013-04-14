586e7f14b1b7f8390cabf1279d4137fd04544f28
meongseo <meong.seo@mail.mcgill.ca>
2013-04-13 23:24:09 -0400

    Javadoc updated

--- AMBroSIA/src/mapObjects/MapObjectTTL.java
+++ AMBroSIA/src/mapObjects/MapObjectTTL.java
@@ -4,10 +4,12 @@ import game.GameState;
 import java.awt.Color;
 
 /**
- * A simple game object, similar to MapObject, except
- * with a color attribute that defines the object's
- * color and a Time To Live attribute that defines
- * how long the object remains on screen for.
+ * The
+ * <code>MapObjectTTL</code> class is a simple game object, similar to
+ * MapObject, except with a color attribute that defines the object's color and
+ * a Time To Live attribute that defines how long the object remains on screen
+ * for.
+ *
  * @author Michael
  */
 public class MapObjectTTL extends MapObject {
@@ -57,8 +59,8 @@ public class MapObjectTTL extends MapObject {
     }
 
     /**
-     * Sets the Time to Live property, in units of 200msec.
-     * (i.e. TTL = 5 would be 1 second)
+     * Sets the Time to Live property, in units of 200msec. (i.e. TTL = 5 would
+     * be 1 second)
      *
      * @param ttl new time to live value
      */
